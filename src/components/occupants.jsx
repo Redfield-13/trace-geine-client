af1bbae6b41ac36ee0ac6dd763227515fad5fdd2
Redfield-13 <fieldr62@gmail.com>
2023-08-04 19:13:31 +0200

    update

--- src/components/occupants.jsx
+++ src/components/occupants.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const Occupants = (props) => {
-    // const occupants = { props };
     console.log(Object.entries(JSON.parse(props["occupants"])));
   return (
     <div className="occupants-container">
       <h3 className="occupants-heading"></h3>
       <div className="occupants-list">
-        
       <ul>
           {Object.entries(JSON.parse(props["occupants"])).map(([key, value]) => (
             <li key={key}>
